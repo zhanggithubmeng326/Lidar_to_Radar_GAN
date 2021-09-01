55e887c9cbe5804690244ef7f19a82524c978c4f
Meng Zhang <zhang4@fzi.de>
2021-09-01 16:36:08 +0200

    finished the build of network

--- netwoks.py
+++ netwoks.py
@@ -109,7 +109,7 @@ class LocalEnhancer(tf.keras.Model):
         self.model_downsampling = model_downsampling
 
         # residual blocks
-        model_upsampling = tf.keras.Sequential()
+        model_upsampling = tf.keras.Sequential()         # always give input_shape firstly???
         for _ in range(local_residual_blocks):
             model_upsampling.add(ResidualBlock(channels_out=base_channels * 4))
 
@@ -132,8 +132,72 @@ class LocalEnhancer(tf.keras.Model):
 
         return output
 
-# build discriminator
+# build a PatchGAN discriminator, which can be used by different scales
 class Discriminator(tf.keras.Model):
 
-    def __init__(self, base_channels=64, n_layers=3):
+    def __init__(self, input_nc, base_channels=64):
         super(Discriminator, self).__init__()
+
+        model_Dis = tf.keras.Sequential()
+
+        # initial convolution layer
+        layer1 = [layers.InputLayer([None, None, input_nc]),
+                  layers.Conv2D(base_channels, kernel_size=4, strides=2, padding='same', kernel_initializer=kernel_initializer),
+                  layers.LeakyReLU(0.2)]
+        model_Dis.add(layer1)
+
+        # downsampling convolutional layer
+        layer2 = [layers.Conv2D(2 * base_channels, kernel_size=4, strides=2, padding='same', kernel_initializer=kernel_initializer),
+                  tfa.layers.InstanceNormalization(axis=-1, center=False, scale=False),
+                  layers.LeakyReLU(0.2)]
+        model_Dis.add(layer2)
+
+        # downsampling convolutional layer
+        layer3 = [layers.Conv2D(4 * base_channels, kernel_size=4, strides=2, padding='same', kernel_initializer=kernel_initializer),
+                  tfa.layers.InstanceNormalization(axis=-1, center=False, scale=False),
+                  layers.LeakyReLU(0.2)]
+        model_Dis.add(layer3)
+
+        # output convolutional layer
+        layer4 = [layers.Conv2D(8 * base_channels, kernel_size=4, strides=1, padding='same', kernel_initializer=kernel_initializer),
+                  tfa.layers.InstanceNormalization(axis=-1, center=False, scale=False),
+                  layers.LeakyReLU(0.2),
+                  layers.Conv2D(1, kernel_size=4, strides=1, padding='same', kernel_initializer=kernel_initializer)]
+        model_Dis.add(layer4)
+        self.model_Dis = model_Dis
+
+        for i in range(len(model_Dis.layers)):
+            setattr(self, 'layer'+str(i), model_Dis.layers[i])
+
+    def call(self, x, training=None, mask=None):
+        layer_result = []
+        for j in range(len(self.model_Dis.layers)):
+            layer = getattr(self, 'layer'+str(j))
+            x = layer(x)
+            layer_result.append(x)
+
+        return layer_result
+
+# build a multi-scale discriminator for three different image resolutions
+class MultiscaleDiscriminator(tf.keras.Model):
+
+    def __init__(self, input_nc, base_channels=64, n_discrimintors=3, ):
+        super(MultiscaleDiscriminator, self).__init__()
+
+        # initialize all three discriminators
+        self.list_discriminator = []
+        for dis in range(n_discrimintors):
+            self.list_discriminator.append(Discriminator(input_nc, base_channels))
+
+        self.downsample = layers.AveragePooling2D(3, strides=2, padding='same')
+
+    def call(self, x, training=None, mask=None):
+        results = []
+
+        for n, discriminator in enumerate(self.list_discriminator):
+            if n != 0:
+                x = self.downsample(x)
+
+            results.append(discriminator(x))  # list which contains multi-scale discriminator outputs
+
+        return results
