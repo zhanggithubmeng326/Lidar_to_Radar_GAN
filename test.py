9240a0f91801b666f09001db99bb2dcbd0fc6421
Meng Zhang <zhang4@fzi.de>
2021-09-15 17:46:55 +0200

    modofied test.py

--- test.py
+++ test.py
@@ -59,5 +59,5 @@ def dataloader(image_path_lidar, image_path_radar, batch_size, buffer_size):
 
 if __name__ == '__main__':
     img_train, img_test = dataloader(image_path_lidar, image_path_radar, batch_size=1, buffer_size=1608)
-    print(img_train)
-    print(img_test)
+    for img_l, img_r in img_train:
+        print(img_l.shape, img_r.shape)
